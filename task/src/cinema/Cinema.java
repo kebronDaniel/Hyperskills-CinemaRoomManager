d66a6b72ed2178020ebaa52e0e903e66e08f4534
KebronDaniel <kebronjrdaniel@gmail.com>
2023-05-30 11:50:53 +0300

    Finished the arrengement. closes #1

--- task/src/cinema/Cinema.java
+++ task/src/cinema/Cinema.java
@@ -2,6 +2,24 @@ package cinema;
 public class Cinema {
 
     public static void main(String[] args) {
-        // Write your code here
+        int rows = 7;
+        int seats = 8;
+        System.out.println("Cinema:");
+        for (int i = 0; i <= seats; i++) {
+            if (i == 0){
+                System.out.print("  ");
+            } else {
+                System.out.print(i + " ");
+            }
+        }
+        System.out.println();
+
+        for (int i = 1; i <= rows; i++) {
+            System.out.print(i + " ");
+            for (int j = 1; j <= seats; j++) {
+                System.out.print("S ");
+            }
+            System.out.println();
+        }
     }
 }
