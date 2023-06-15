c990ee91a719774d94559ffb4cef60d200fd0898
KebronDaniel <kebronjrdaniel@gmail.com>
2023-06-15 12:39:29 +0300

    Finished task 5, which is to remove errors. closes #5.

--- task/src/cinema/Cinema.java
+++ task/src/cinema/Cinema.java
@@ -1,5 +1,6 @@
 package cinema;
 
+import java.text.DecimalFormat;
 import java.util.Arrays;
 import java.util.Scanner;
 
@@ -24,10 +25,13 @@ public class Cinema {
         String[][] seatArrangement = new String[rows][seats];
         int[] emptySeat = new int[]{0,0};
 
+        int current_income = 0;
+        int numberOfSoldTickets = 0;
+
         while (true){
-            System.out.println(Arrays.deepToString(seatArrangement));
             System.out.println("1. Show the seats");
             System.out.println("2. Buy a ticket");
+            System.out.println("3. Statistics");
             System.out.println("0. Exit");
 
             int choice = scanner.nextInt();
@@ -39,27 +43,63 @@ public class Cinema {
                 showSeats(rows,seats, seatArrangement);
                 System.out.println();
             } else if (choice == 2) {
-                System.out.println();
-                System.out.println("Enter a row number:");
-                rowNumber = scanner.nextInt();
-                System.out.println("Enter a seat number in that row:");
-                seatNumber = scanner.nextInt();
 
-                seatArrangement[rowNumber - 1][seatNumber - 1] = " B";
+                while (true){
+                    System.out.println();
+                    System.out.println("Enter a row number:");
+                    rowNumber = scanner.nextInt();
+                    System.out.println("Enter a seat number in that row:");
+                    seatNumber = scanner.nextInt();
 
-                System.out.println(Arrays.deepToString(seatArrangement));
+                    if (rowNumber > rows || seatNumber > seats){
+                        System.out.println("Wrong input!");
+                    }else {
+                        if (seatArrangement[rowNumber - 1][seatNumber - 1] == " B"){
+                            System.out.println();
+                            System.out.println("That Ticket has already been purchased!");
+                            System.out.println();
 
-                System.out.println();
-                calculateSeatPrice(rows, seats,rowNumber);
+                        }else {
+                            seatArrangement[rowNumber - 1][seatNumber - 1] = " B";
+                            System.out.println();
+
+                            int price = calculateSeatPrice(rows,seats,rowNumber);
+                            current_income += price;
+
+                            System.out.println("Ticket price: $" +price);
+                            System.out.println();
+                            numberOfSoldTickets++;
+                            break;
+                        }
+                    }
+                }
+
+            } else if (choice == 3) {
+                System.out.println("Number of purchased tickets: " + numberOfSoldTickets);
+
+                double ticktes = numberOfSoldTickets;
+                double totalTickets = rows * seats;
+                double percentage = (ticktes / totalTickets) * 100; // The above two were made to have doubles than ints.
+
+                System.out.println("total seats : " + rows * seats);
+                System.out.println("Sold tickets : " + numberOfSoldTickets);
+                DecimalFormat decimalFormat = new DecimalFormat("0.00");
+                System.out.println("Percentage: " + decimalFormat.format(percentage) + "%");
+                System.out.println("Current income: $" + current_income);
+                System.out.println("Total income: $" + calculateTotalIncome(rows,seats));
                 System.out.println();
             } else if (choice == 0) {
                 break;
+            }else {
+                System.out.println();
+                System.out.println("Wrong input!");
+                System.out.println();
             }
         }
 
     }
 
-    private static void calculateSeatPrice(int rows, int seats,int rowNumber){
+    private static int calculateSeatPrice(int rows, int seats,int rowNumber){
         int price = 0;
         if (rows * seats <= 60){
             price = 10;
@@ -70,7 +110,22 @@ public class Cinema {
                 price = 8;
             }
         }
-        System.out.println("Ticket price: $" +price);
+        return price;
+    }
+
+    private static int calculateTotalIncome(int rows, int seats){
+        int totalIncome = 0;
+        int firstHalf = 0;
+        int secondHalf = 0;
+        if (rows * seats <= 60){
+            totalIncome = (rows * seats) * 10;
+            return totalIncome;
+        } else {
+            firstHalf = ((rows / 2) * seats) * 10;
+            secondHalf = ((rows - (rows/2)) * seats) * 8;
+            totalIncome = firstHalf + secondHalf;
+            return totalIncome;
+        }
     }
 
     private static void showSeats(int rows, int seats, String[][]seatArrangement) {
